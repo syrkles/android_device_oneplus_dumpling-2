26f4d6e0a24c3083f176f3757d4b9a4e33d509c2
Sultanxda <sultanxda@gmail.com>
2016-07-09 16:38:57 -0700

    dumpling: Add custom power HAL
    This simple power HAL will configure the "sys.perf.profile" property upon
    receiving performance-profile change requests, so that init can parse
    "sys.perf.profile" and make appropriate changes. This allows for greater
    customization and simplicity when configuring performance profiles.
    
    Supported power modes: powersave, balanced, performance.
    
    Also features the tap-to-wake feature from upstream.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,5 +35,8 @@ DEVICE_PATH := device/oneplus/dumpling
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
+# Power
+TARGET_POWERHAL_VARIANT := none
+
 # inherit from the proprietary version
 -include vendor/oneplus/dumpling/BoardConfigVendor.mk
